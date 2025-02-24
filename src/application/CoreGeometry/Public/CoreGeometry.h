1669be93d9b95ed48900f107f794767e92101e73
Xinghua Han <vlairrrrr@outlook.com>
2025-02-24 16:10:44 +0800

    feat: add obj model IO in CoreGeometry

---
+++ src/application/CoreGeometry/Public/CoreGeometry.h
@@ -0,0 +1,110 @@
+#pragma once
+
+#ifdef COREGEOMETRY_IMPLEMENT
+#define COREGEOMETRY_API __declspec(dllexport)
+#else
+#ifdef COREGEOMETRY_INCLUDE
+#define COREGEOMETRY_API __declspec(dllimport)
+#else
+#error Please specify API linkage before include this file
+#define COREGEOMETRY_API
+#endif
+#endif
+#include <string>
+#include <vector>
+
+#include "tiny_obj_loader.h"
+
+namespace CoreGeometryImpl
+{
+	bool COREGEOMETRY_API TinyObjLoadObj(tinyobj::attrib_t *attrib, std::vector<tinyobj::shape_t> *shapes,
+             std::vector<tinyobj::material_t> *materials, std::string *err,
+             const char *filename, const char *mtl_basedir = nullptr,
+             bool triangulate = true);
+}
+
+
+template<typename PositionType, typename ColorType, typename TexCoordType, typename NormalType, typename TangentType>
+struct Vertex {
+    PositionType Position;
+    ColorType Color;
+    TexCoordType TexCoord;
+    NormalType Normal;
+    TangentType TangentTypeX;
+    TangentType TangentTypeY;
+};
+
+template<typename VertexType, typename IndexType>
+class TMesh
+{
+public:
+    std::vector<VertexType> Vertices;
+    std::vector<IndexType> Indices;
+
+    static TMesh<VertexType, IndexType> LoadObj(const std::string ObjFileName);
+};
+
+
+template <typename VertexType, typename IndexType>
+TMesh<VertexType, IndexType> TMesh<VertexType, IndexType>::LoadObj(const std::string ObjFileName)
+{
+	TMesh<VertexType, IndexType> OutMesh;
+	tinyobj::attrib_t attrib;
+    std::vector<tinyobj::shape_t> shapes;
+    std::vector<tinyobj::material_t> materials;
+    std::string warn, err;
+
+    if (!CoreGeometryImpl::TinyObjLoadObj(&attrib, &shapes, &materials, &err, ObjFileName.c_str(), nullptr, false)) {
+        throw std::runtime_error(warn + err);
+    }
+
+    for (const auto& shape : shapes) {
+        for (const auto& num_verts : shape.mesh.num_face_vertices) {
+            if(num_verts!=3)
+            {
+	            throw std::runtime_error("Can only support triangulate mesh!");
+            }
+        }
+    }
+
+    uint32_t CurrentIndex = 0;
+    for (const auto& shape : shapes) {
+        for (const auto& index : shape.mesh.indices) {
+            VertexType vertex{};
+	        vertex.Position = {
+	        	attrib.vertices[3 * index.vertex_index],
+	        	attrib.vertices[3 * index.vertex_index + 1],
+	        	attrib.vertices[3 * index.vertex_index + 2]};
+	        if(attrib.normals.size()>0)
+	        {
+				vertex.Normal = {
+					attrib.normals[3 * index.normal_index],
+					attrib.normals[3 * index.normal_index + 1],
+					attrib.normals[3 * index.normal_index + 2]};
+	        }
+    		else
+	        {
+		        vertex.Normal = {};
+	        }
+
+	        if(attrib.texcoords.size()>0)
+	        {
+				vertex.TexCoord = {
+					attrib.texcoords[2 * index.texcoord_index],
+					attrib.texcoords[2 * index.texcoord_index + 1]};
+	        }
+    		else
+	        {
+		        vertex.TexCoord = {};
+	        }
+
+	        vertex.Color = {};
+	        vertex.TangentTypeX = {};
+	        vertex.TangentTypeY = {};
+            OutMesh.Vertices.push_back(vertex);
+            OutMesh.Indices.push_back(CurrentIndex++);
+        }
+    }
+
+    return OutMesh;
+}
--- src/application/Engine/Private/Engine.cpp
+++ src/application/Engine/Private/Engine.cpp
@@ -1,15 +1,15 @@
 #define RHI_INCLUDE
+#define COREGEOMETRY_INCLUDE
 #include <chrono>
 
 #include "RHI.h"
 
 #include "CoreMath.h"
 #include "CoreMath.inl"
+#include "CoreGeometry.h"
 
-#define TINYOBJLOADER_IMPLEMENTATION
 #include <unordered_map>
 
-#include "tiny_obj_loader.h"
 #include "GLFW/glfw3.h"
 #include "glm/glm.hpp"
 
@@ -38,65 +38,6 @@ struct UniformBufferObject {
     alignas(16) float4x4 view;
     alignas(16) float4x4 proj;
 };
-
-struct Vertex {
-    float3 pos;
-    float3 color;
-    float2 texCoord;
-
-    bool operator==(const Vertex& other) const {
-        return pos == other.pos && color == other.color && texCoord == other.texCoord;
-    }
-};
-
-namespace std {
-    template<> struct hash<Vertex> {
-        size_t operator()(Vertex const& vertex) const {
-            return ((hash<float3>()(vertex.pos) ^ (hash<float3>()(vertex.color) << 1)) >> 1)
-                ^ (hash<float2>()(vertex.texCoord) << 1);
-        }
-    };
-}
-
-
-void loadModel(std::vector<Vertex>& OutVertices, std::vector<uint32_t>& OutIndices, const std::string infile) {
-    tinyobj::attrib_t attrib;
-    std::vector<tinyobj::shape_t> shapes;
-    std::vector<tinyobj::material_t> materials;
-    std::string warn, err;
-
-    if (!tinyobj::LoadObj(&attrib, &shapes, &materials, &err, infile.c_str(), nullptr, false)) {
-        throw std::runtime_error(warn + err);
-    }
-
-    std::unordered_map<Vertex, uint32_t> uniqueVertices{};
-
-    for (const auto& shape : shapes) {
-        for (const auto& index : shape.mesh.indices) {
-            Vertex vertex{};
-
-            vertex.pos = {
-                attrib.vertices[3 * index.vertex_index + 0],
-                attrib.vertices[3 * index.vertex_index + 1],
-                attrib.vertices[3 * index.vertex_index + 2]
-            };
-
-            vertex.texCoord = {
-                attrib.texcoords[2 * index.texcoord_index + 0],
-                1.0f - attrib.texcoords[2 * index.texcoord_index + 1]
-            };
-
-            vertex.color = { 1.0f, 1.0f, 1.0f };
-
-            if (uniqueVertices.count(vertex) == 0) {
-                uniqueVertices[vertex] = static_cast<uint32_t>(OutVertices.size());
-                OutVertices.push_back(vertex);
-            }
-
-            OutIndices.push_back(uniqueVertices[vertex]);
-        }
-    }
-}
 
 std::vector<char> readFile(const std::string& filename) {
     std::ifstream file(filename, std::ios::ate | std::ios::binary);
@@ -130,13 +71,10 @@ void updateUniformBuffer(UniformBufferObject& OutUniformBufferObject) {
 
 int main()
 {
-	// Init RHI Context & window
-	// create pipeline
-	// create buffers
-	// set rendering parameters
-	// executing shaders
-    std::vector<Vertex> vertices;
-    std::vector<uint32_t> indices;
+    typedef Vertex<float3, float3, float2, float3, float3> VertexType;
+    typedef TMesh<VertexType, uint32_t> MeshType;
+
+    MeshType StaticMesh = MeshType::LoadObj(MODEL_PATH);
     UniformBufferObject ubo;
     // RHI Objects
     RHIVulkanContext Context({WIDTH, HEIGHT});
@@ -149,26 +87,19 @@ int main()
 
     std::vector<char> VertexShaderSPIRV = readFile(VERT_SHADER_PATH);
     std::vector<char> FragmentShaderSPIRV = readFile(FRAG_SHADER_PATH);
-    loadModel(vertices, indices, MODEL_PATH);
-    vertices.push_back(Vertex{ float3(-1., 0.,0.), float3(1.,0.,0.), float2(0.,0.) });
-    vertices.push_back(Vertex{ float3( 1., 0.,0.), float3(0.,1.,0.), float2(1.,0.) });
-    vertices.push_back(Vertex{ float3( 0., 1.,0.), float3(1.,0.,1.), float2(0.,1.) });
-    indices.push_back(0);
-    indices.push_back(2);
-    indices.push_back(1);
 
     Context.Initialize();
     Uniform.Initialize(&Context, sizeof(UniformBufferObject));
-    RHIVertexBuffer.Initialize(&Context, sizeof(Vertex), vertices.size(), VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_VERTEX_BUFFER_BIT, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT);
-    RHIIndexBuffer.Initialize(&Context, sizeof(Vertex), vertices.size(), VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_INDEX_BUFFER_BIT, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT);
-    RHIVertexBuffer.CopyToBuffer(&Context, vertices.data(), vertices.size() * sizeof(Vertex));
-    RHIIndexBuffer.CopyToBuffer(&Context, indices.data(), indices.size() * sizeof(uint32_t));
+    RHIVertexBuffer.Initialize(&Context, sizeof(VertexType), StaticMesh.Vertices.size(), VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_VERTEX_BUFFER_BIT, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT);
+    RHIIndexBuffer.Initialize(&Context, sizeof(VertexType), StaticMesh.Vertices.size(), VK_BUFFER_USAGE_TRANSFER_DST_BIT | VK_BUFFER_USAGE_INDEX_BUFFER_BIT, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT);
+    RHIVertexBuffer.CopyToBuffer(&Context, StaticMesh.Vertices.data(), StaticMesh.Vertices.size() * sizeof(VertexType));
+    RHIIndexBuffer.CopyToBuffer(&Context, StaticMesh.Indices.data(), StaticMesh.Indices.size() * sizeof(uint32_t));
     Texture.Initialize(&Context, TEXTURE_PATH.c_str());
 
-    Pipeline.AddBinding(0, sizeof(Vertex));
-    Pipeline.AddLayout(0, 0, VK_FORMAT_R32G32B32_SFLOAT, offsetof(Vertex, pos));
-    Pipeline.AddLayout(0, 1, VK_FORMAT_R32G32B32_SFLOAT, offsetof(Vertex, color));
-    Pipeline.AddLayout(0, 2, VK_FORMAT_R32G32_SFLOAT, offsetof(Vertex, texCoord));
+    Pipeline.AddBinding(0, sizeof(VertexType));
+    Pipeline.AddLayout(0, 0, VK_FORMAT_R32G32B32_SFLOAT, offsetof(VertexType, Position));
+    Pipeline.AddLayout(0, 1, VK_FORMAT_R32G32B32_SFLOAT, offsetof(VertexType, Color));
+    Pipeline.AddLayout(0, 2, VK_FORMAT_R32G32_SFLOAT, offsetof(VertexType, TexCoord));
     Pipeline.AddUniformBuffer({ Uniform.Buffer, 0, sizeof(UniformBufferObject) });
     Pipeline.AddImageSampler({ Texture.Sampler, Texture.ImageView, VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL });
     Pipeline.Initialize(&Context, VertexShaderSPIRV, FragmentShaderSPIRV);
@@ -180,7 +111,7 @@ int main()
     {
         updateUniformBuffer(ubo);
         Uniform.CopyToBuffer(&Context, &ubo, sizeof(ubo));
-        GraphicDispatcher.Dispatch(&Context, &Pipeline, indices.size(), 0, 1);
+        GraphicDispatcher.Dispatch(&Context, &Pipeline, StaticMesh.Indices.size(), 0, 1);
     }
 
 	return 0;
